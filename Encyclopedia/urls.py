69541655587594bff339b36ab002c272c9b3dc64
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-28 21:50:38 +0530

    Created different url paths for specific operations

--- Encyclopedia/urls.py
+++ Encyclopedia/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = "Encyclopedia"
 urlpatterns = [
-     path('', views.index, name="index")
+     path('', views.index, name="index"),
+     path('contents', views.content, name="content"),
+     path('random', views.random, name="random"),
+     path('create', views.create, name="create"),
+     path('wiki/<str:reqPage>', views.wiki, name="wiki")
 ]
--- Encyclopedia/views.py
+++ Encyclopedia/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        return HttpResponseRedirect(f"/{request.POST.get('q')}")
+        return HttpResponseRedirect(f"wiki/{request.POST.get('q')}")
     return render(request, "Encyclopedia/index.html")
+
+def content(request):
+    return HttpResponse("Content Page") 
+
+def create(request):    
+    return HttpResponse("Create Page")
+
+def random(request):    
+    return HttpResponse("Random Page")
+
+def wiki(request, reqPage):    
+    return HttpResponse("Wiki Page")
