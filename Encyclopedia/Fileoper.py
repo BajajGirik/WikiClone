16f0b94bf928863c0d01f642853292facb0fed76
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-28 23:41:35 +0530

    Started writing file functions

--- Encyclopedia/Fileoper.py
+++ Encyclopedia/Fileoper.py
@@ -1,2 +1,12 @@
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+
+def createFile(title, content):
+    path = f"./Files/{title}.md"
+
+    if default_storage.exists(path):
+        default_storage.delete(path)
+
+    default_storage.save(path, ContentFile(content))  
+     
+
