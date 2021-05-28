4a31fd4d1d2d2b6121aa298277fa141febe2b17f
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-28 21:00:08 +0530

    Added HttpRedirect after form is submitted

--- Encyclopedia/views.py
+++ Encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        return HttpResponseRedirect(f"/{request.POST.get('q')}")
     return render(request, "Encyclopedia/index.html")
