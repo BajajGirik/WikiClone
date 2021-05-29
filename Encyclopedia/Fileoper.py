5c71db5f3013615b61d8bde55dd0f27c43050cec
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-29 14:14:51 +0530

    Implemented file functions

--- Encyclopedia/Fileoper.py
+++ Encyclopedia/Fileoper.py
@@ -2,11 +2,20 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
 def createFile(title, content):
-    path = f"./Files/{title}.md"
+    path = f"Files/{title}.md"
 
     if default_storage.exists(path):
         default_storage.delete(path)
 
     default_storage.save(path, ContentFile(content))  
      
+def getFile(title):
+    path = f"Files/{title}.md"
+    if default_storage.exists(path):
+        return default_storage.open(path).read().decode("utf-8")
+    else:
+        return None
 
+def totalFiles():
+    direc, filenames = default_storage.listdir("Files")
+    return len(filenames)            
--- Encyclopedia/views.py
+++ Encyclopedia/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from . import Fileoper
 
 # Create your views here.
 def index(request):
+    total = Fileoper.totalFiles()
     if request.method == 'POST':
         return HttpResponseRedirect(f"wiki/{request.POST.get('q')}")
-    return render(request, "Encyclopedia/index.html")
+    return render(request, "Encyclopedia/index.html", {
+        "total": total
+    })
 
 def content(request):
     return HttpResponse("Content Page") 
