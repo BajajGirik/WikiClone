a9e6ff778152a8c1537846a38f6132e2a87c61cd
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-31 16:38:04 +0530

    Added remove of alerts on clicking

--- Encyclopedia/static/Encyclopedia/index.js
+++ Encyclopedia/static/Encyclopedia/index.js
@@ -5,7 +5,14 @@ checkEnter = (e) => {
     }
 }   
 
+removeIt = (e) => {
+    e.target.parentElement.style.display = "none";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    var x = document.querySelector("#Main__content__create__input");
+    const x = document.querySelector("#Main__content__create__input");
     x.onkeypress = checkEnter;
+    
+    const y = document.querySelector("#Message__remove");
+    y.onclick = removeIt;
 });
