1a3a62e5d4e41a0e6beb975d68484bb8fd4671dc
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-01 15:02:11 +0530

    Added a new model

--- Encyclopedia/models.py
+++ Encyclopedia/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class filenameloc(models.Model):
+    title = models.CharField(max_length=64)
+    path = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.title}"
